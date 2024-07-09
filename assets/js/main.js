e42f3389c7263e3645fe23175b0ed21bf294d630
NeiJunio <neijunio50@gmail.com>
2024-07-09 15:34:57 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
 
@@ -6,10 +5,20 @@ const maxRecords = 151; // Limitar somente na 1ª geração dos pokemons
 const limit = 10;
 let offset = 0;
 
+//--------
+let allPokemons = [];
+//--------
+
 function loadPokemonItems(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map((pokemon) => 
-            `<li class="pokemon ${pokemon.type}" data-number="${pokemon.number}">
+
+        //--------
+        allPokemons = [...allPokemons, ...pokemons];//Os novos Pokémon carregados são concatenados ao array global allPokemons a cada chamada de loadPokemonItems.
+        //--------
+
+        //adiciondno um index para identificar cada pokemom (index)
+        const newHtml = pokemons.map((pokemon, index) =>
+            `<li class="pokemon ${pokemon.type}" data-index="${offset + index}">
                 <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
@@ -22,12 +31,19 @@ function loadPokemonItems(offset, limit) {
         ).join('');
 
         pokemonList.innerHTML += newHtml;
-        
+
         // Adicionar event listeners para os novos elementos adicionados
         const pokemonElements = pokemonList.querySelectorAll('.pokemon');
-        pokemonElements.forEach((element, index) => {
-            const pokemon = pokemons[offset + index];
-            element.addEventListener('click', () => openModal(pokemon));
+
+        pokemonElements.forEach((element) => {
+            element.addEventListener('click', () => {
+
+                //referenciando o pokemon ao array global com todos os poklemons
+                const pokemonIndex = element.getAttribute('data-index');
+                const pokemon = allPokemons[pokemonIndex];
+                openModal(pokemon)
+
+            });
         });
     });
 }
@@ -36,20 +52,17 @@ loadPokemonItems(offset, limit);
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit;
-    
+
     const qtRecordNextPage = offset + limit;
-    
+
     if (qtRecordNextPage >= maxRecords) {
         const newLimit = maxRecords - offset;
         loadPokemonItems(offset, newLimit);
-        
-        
+
+
         // Remover o botão "load more" quando atingir o limite máximo
         loadMoreButton.parentElement.removeChild(loadMoreButton);
     } else {
         loadPokemonItems(offset, limit);
     }
 });
-
-
-
